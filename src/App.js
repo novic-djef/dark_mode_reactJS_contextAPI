d5b6b54d89ff979b687b999be9fa603d4121c803
unknown <novicmelataguia@yahoo.com>
2023-01-14 03:01:32 +0100

    contextAPI applique pour le dark mode react.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorModeContext, useMode } from "./them";
+import { CssBaseline, ThemeProvider }  from '@mui/material';
+import Topbar from './screns/globale/topbar'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [theme, colorMode] = useMode();
+  return ( 
+  <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> 
+        <div className="app">
+          <main className="contnet">
+             <Topbar />
+          </main>
+        </div>;
+      </ThemeProvider>
+  </ColorModeContext.Provider> 
+  
   );
 }
 
---
+++ src/data/mockData.js
@@ -0,0 +1,35 @@
+
+export const mockDataTeam = [
+    {
+        id: 1,
+        name: 'novic-Djef',
+        email: 'nono@yahoo.com',
+        age: 12,
+        phone: '(+237)690-089-951',
+        access: 'admin',
+    },
+    {
+        id: 2,
+        name: 'papa',
+        email: 'papa@yahoo.com',
+        age: 42,
+        phone: '(+237)690-089-951',
+        access: 'manager',
+    },
+    {
+        id: 3,
+        name: 'Maman',
+        email: 'Maman@yahoo.com',
+        age: 37,
+        phone: '(+237)690-089-951',
+        access: 'admin',
+    },
+    {
+        id: 4,
+        name: 'novic-Djef',
+        email: 'nono@yahoo.com',
+        age: 12,
+        phone: '(+237)690-089-951',
+        access: 'admin',
+    },
+]
